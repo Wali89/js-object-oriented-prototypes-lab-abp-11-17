3e200ed8c38577ccffe23ca43bfa54337434693b
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-17 02:12:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState
   this.training = training
 
-   this.veto = function(){
-     return 'No, I must disagree'
+   //this.veto = function(){
+  //   return 'No, I must disagree'
    }
-  this.approve = function(){
-     return 'You can do that!'
-   }
-   this.doCharity = function(){
-     return "I like to help people."
-   }
+//  this.approve = function(){
+//     return 'You can do that!'
+//   }
+//   this.doCharity = function(){
+//     return "I like to help people."
+//   }
 
-   this.releasePressStatement = function(){
-     return "You will see great things from Scuber."
-   }
+  // this.releasePressStatement = function(){
+  //   return "You will see great things from Scuber."
+  // }
 
-   this.sayHi = function(){
+  // this.sayHi = function(){
      return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
    }
  }
