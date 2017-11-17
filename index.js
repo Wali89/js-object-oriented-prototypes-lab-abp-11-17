39942f1865ccb63604bbf508735be0e49610b1f3
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-17 02:16:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState
   this.training = training
 
-  
+
  }
 
  BoardMember.prototype.veto = function() {
    return('No, I must disagree');
  }
+
+BoardMember.prototype.approve = function() {
+  return('You can do that');
+}
+
+BoardMember.prototype.doCharity = function() {
+  return('I like to help people.');
+}
+
+BoardMember.prototype
